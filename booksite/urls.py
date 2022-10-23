3083bd30300331190d8c5ec02fa437e5b2da8aec
[AtharvPorate1] <www.atharvporate254@gmail.com>
2022-10-23 13:00:33 +0530

    for deployment 2

--- booksite/urls.py
+++ booksite/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.conf.urls import urls
+# from django.conf.urls import urls
 
 
 
@@ -16,8 +16,8 @@ urlpatterns = [
     path('chat/', include('base.urls'),name="chat"),
     path('news/', include('news_api.urls'),name="news"),
     path('music/', include('music.urls'),name="music"),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
 ]
