80cf931b49224c808fdd765d699445730b7d07fd
[AtharvPorate1] <www.atharvporate254@gmail.com>
2022-10-23 07:35:21 +0530

    finalizing project

--- booksite/urls.py
+++ booksite/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage),
     path('book/', include('bookapp.urls')),
     path('chat/', include('base.urls')),
     path('news/', include('news_api.urls')),
