5f0ee01ecb19834d9a1687ed6643e1e63315e2a0
[AtharvPorate1] <www.atharvporate254@gmail.com>
2023-05-28 12:39:59 +0530

    fixed landing page error

--- booksite/urls.py
+++ booksite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+# from . import views
 # from django.conf.urls import urls
 
 
@@ -11,7 +11,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    # path('', views.homepage),
     path('book/', include('bookapp.urls')),
     path('chat/', include('base.urls'),name="chat"),
     path('news/', include('news_api.urls'),name="news"),
